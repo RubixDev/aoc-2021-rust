7b8b4943fb2d9c1fbc2d8df4504484e2b7d83532
RubixDev <silas.groh@t-online.de>
2021-12-01 11:31:39 +0100

    Day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,16 +1,41 @@
 pub fn run() {
     println!("--- DAY 01 ---");
-    let input = vec![2, 3]; //include_str!("resources/input01.txt");
+    let input: Vec<u16> = include_str!("resources/input01.txt")
+        .split('\n')
+        .map(|num| num.parse::<u16>().unwrap())
+        .collect();
     part1(&input);
     println!();
     part2(&input);
     println!();
 }
 
-fn part1(input: &Vec<u32>) {
-    println!("{:?}", input)
+fn part1(input: &Vec<u16>) {
+    println!(
+        "{}",
+        input
+            .iter()
+            .enumerate()
+            .filter(|(index, num)| index != &0 && num > &&input[index - 1])
+            .count()
+    );
 }
 
-fn part2(input: &Vec<u32>) {
-    println!("{:?}", input)
+fn of_index(input: &Vec<u16>, index: usize) -> u16 {
+    if index > input.len() - 3 {
+        0
+    } else {
+        input[index] + input[index + 1] + input[index + 2]
+    }
+}
+
+fn part2(input: &Vec<u16>) {
+    println!(
+        "{}",
+        input
+            .iter()
+            .enumerate()
+            .filter(|(index, _)| index != &0 && of_index(input, *index) > of_index(input, index - &1))
+            .count()
+    );
 }
